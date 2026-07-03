e2f5fc7ad58faee052a9e9876d950983817099d9
DandelionSeedlings <dandelionseedlings@outlook.com>
2026-07-03 09:16:42 +0200

    Add /get-connectability redirect

--- next.config.js
+++ next.config.js
@@ -6,5 +6,15 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/get-connectability',
+        destination: 'https://script.google.com/macros/s/AKfycbz_b7hNBW35xJ9XhX_6xI0v3aLH87vsPAGNxSSfaf4mIXAT0yckCI9T7RlqPoh1zEPK/exec',
+        permanent: false,
+      },
+    ];
+  },
 }
 module.exports = nextConfig
