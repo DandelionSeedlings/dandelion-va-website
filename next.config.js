b1ecef2dda652c7b92ef7f31837463c10bc003db
DandelionSeedlings <dandelionseedlings@outlook.com>
2026-07-03 08:47:22 +0200

    Revert "Add /get-connectability redirect to free CRM form"
    This reverts commit b3b270f6356c6e2d2b713e8c7a618bc56768d811.

--- next.config.js
+++ next.config.js
@@ -6,15 +6,5 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-
-  async redirects() {
-    return [
-      {
-        source: '/get-connectability',
-        destination: 'https://script.google.com/macros/s/AKfycbz_b7hNBW35xJ9XhX_6xI0v3aLH87vsPAGNxSSfaf4mIXAT0yckCI9T7RlqPoh1zEPK/exec',
-        permanent: false,
-      },
-    ];
-  },
 }
 module.exports = nextConfig
