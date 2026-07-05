09d3f63a3a3897af0c7d064a134b42fc341552ba
DandelionSeedlings <dandelionseedlings@outlook.com>
2026-07-05 19:14:45 +0200

    Fix sitemap import path

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,15 +1,13 @@
-import { getAllSlugs } from './lib/blog'
+import { getAllSlugs } from '../lib/blog'
 
 export default async function sitemap() {
   const baseUrl = 'https://dandelioncreations.co.za'
   
-  // Static routes
   const staticRoutes = [
     { url: baseUrl, lastModified: new Date(), changeFrequency: 'weekly', priority: 1.0 },
     { url: `${baseUrl}/blog`, lastModified: new Date(), changeFrequency: 'weekly', priority: 0.8 },
   ]
 
-  // Blog posts
   const slugs = getAllSlugs()
   const blogPosts = slugs.map((slug) => ({
     url: `${baseUrl}/blog/${slug}`,
